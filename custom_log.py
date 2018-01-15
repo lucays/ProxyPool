0dbe2c045ec6838c41809e8e158b82388041a1e3
luca <lucahhai@gmail.com>
2018-01-15 22:23:39 +0800

    add

---
+++ custom_log.py
@@ -0,0 +1,63 @@
+import os
+import logging.config
+
+# 配置log
+standard_format = '[%(asctime)s][%(threadName)s:%(thread)d][task_id:%(name)s][%(filename)s:%(lineno)d]' \
+                  '[%(levelname)s][%(message)s]'  # 其中name为getlogger指定的名字
+simple_format = '[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d]%(message)s'
+id_simple_format = '[%(levelname)s][%(asctime)s] %(message)s'
+
+# 定义日志输出格式 结束
+logfile_dir = os.path.dirname(os.path.abspath(__file__)) + '/logs'  # log文件的目录
+logfile_name = 'proxy.log'  # log文件名
+
+# 如果不存在定义的日志目录就创建一个
+if not os.path.isdir(logfile_dir):
+    os.mkdir(logfile_dir)
+
+# log文件的全路径
+logfile_path = os.path.join(logfile_dir, logfile_name)
+
+# log配置字典
+LOGGING_DIC = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': standard_format
+        },
+        'simple': {
+            'format': simple_format
+        },
+    },
+    'filters': {},
+    'handlers': {
+        # 打印到终端的日志
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',  # 打印到屏幕
+            'formatter': 'simple'
+        },
+        # 打印到文件的日志,收集info及以上的日志
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件
+            'formatter': 'standard',
+            'filename': logfile_path,  # 日志文件
+            'maxBytes': 1024*1024*3,  # 日志大小 3M
+            'backupCount': 1,
+            'encoding': 'utf-8',  # 日志文件的编码
+        },
+    },
+    'loggers': {
+        # logging.getLogger(__name__)拿到的logger配置
+        '': {
+            'handlers': ['default', 'console'],  # 这里把上面定义的两个handler都加上，即log数据既写入文件又打印到屏幕
+            'level': 'INFO',
+            'propagate': True,  # 向上（更高level的logger）传递
+        },
+    },
+}
+
+logging.config.dictConfig(LOGGING_DIC)  # 导入上面定义的logging配置
+logger = logging.getLogger(__name__)  # 生成一个log实例
