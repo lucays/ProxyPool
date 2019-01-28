d9439c7c8b74480f79458437431b87804d484fef
luca <lucahhai@gmail.com>
2019-01-29 00:18:11 +0800

    rm 失效站

--- proxies/spiders/spider.py
+++ proxies/spiders/spider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+
 from ..items import ProxiesItem
 
 
@@ -67,51 +68,12 @@ class _3366ipSpider(scrapy.Spider):
             yield item
 
 
-class MimiipSpider(scrapy.Spider):
-    name = 'mimiip'
-    start_urls = [f'http://www.mimiip.com/gngao/{i}' for i in range(1, 3)]
-
-    def parse(self, response):
-        tmp = [i for i in response.xpath('//table/tr/td/text()').extract()]
-        datas = [i for i in tmp if i.isdigit() or ''.join(i.split('.')).isdigit()]
-        for ip, port in zip(datas[::2], datas[1::2]):
-            item = ProxiesItem()
-            item['proxy'] = f'{ip}:{port}'
-            yield item
-
-
 class Data5uSpider(scrapy.Spider):
     name = 'data5u'
     start_urls = ['http://www.data5u.com/free/gngn/index.shtml']
 
     def parse(self, response):
         tmp = [i for i in response.xpath('//ul[@class="l2"]/span/li/text()').extract()]
-        datas = [i for i in tmp if i and (i.isdigit() or ''.join(i.split('.')).isdigit())]
-        for ip, port in zip(datas[::2], datas[1::2]):
-            item = ProxiesItem()
-            item['proxy'] = f'{ip}:{port}'
-            yield item
-
-
-class Ip181Spider(scrapy.Spider):
-    name = 'ip181'
-    start_urls = ['http://www.ip181.com/']
-
-    def parse(self, response):
-        tmp = [i for i in response.xpath('//tbody/tr/td').extract()]
-        datas = [i for i in tmp if i and (i.isdigit() or ''.join(i.split('.')).isdigit())]
-        for ip, port in zip(datas[::2], datas[1::2]):
-            item = ProxiesItem()
-            item['proxy'] = f'{ip}:{port}'
-            yield item
-
-
-class Kxdaili(scrapy.Spider):
-    name = 'kxdaili'
-    start_urls = [f'http://www.kxdaili.com/dailiip/1/{i}.html#ip' for i in range(1, 3)]
-
-    def parse(self, response):
-        tmp = [i for i in response.xpath('//tbody/tr/td').extract()]
         datas = [i for i in tmp if i and (i.isdigit() or ''.join(i.split('.')).isdigit())]
         for ip, port in zip(datas[::2], datas[1::2]):
             item = ProxiesItem()
